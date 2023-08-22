b38c55bbd3387293bdd00e1b103008cdb2894864
AfiMaameDufie <afimaamedufie@gmail.com>
2023-08-22 02:26:56 +0000

    Add Category and Event database models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True) 
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+    
+EVENT_TYPES = [
+    ('CF', 'Conference'),
+    ('SM', 'Summit'),
+    ('HK', 'Hackathon'),
+    ('CJ', 'CodeJam'),
+    ('EX', 'Expo'),
+    ('SC', 'StartupCompetition'),
+    ('DC', 'DeveloperConference'),
+    ('WS', 'Workshop'),
+    ('MT', 'Meetup'),
+    ('UG', 'UserGroup'),
+    ('OC', 'OnlineConferences'),
+    ('WB', 'Webinar'),
+]
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=100)
+    event_type = models.CharField(max_length=2, choices=EVENT_TYPES, default='')
+    categories = models.ManyToManyField(to=Category, blank=True, related_name='categories')
+    duration = models.DateTimeField(null=True)
+    country = models.CharField(max_length=40, null=True)
+    location = models.CharField(max_length=50, null=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.event_name}, {self.event_type}, {self.country}'
