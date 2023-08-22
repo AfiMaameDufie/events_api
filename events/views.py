0d51f71b94a3c3e0fa1d65710cc32eb2d57ffcc6
AfiMaameDufie <afimaamedufie@gmail.com>
2023-08-22 02:44:17 +0000

    Added viewsets for events models

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from events.models import Category, Event
+from events.serializers import CategorySerializer, EventSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
